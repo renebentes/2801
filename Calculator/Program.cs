d5bbfb3a4ce4d16babda053a562e7d8c377ed636
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-08-11 19:56:38 -0300

    Inclui operação de multiplicação

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -53,3 +53,21 @@ static void Divisao()
 
     Console.ReadKey();
 }
+
+static void Multiplicacao()
+{
+    Console.Clear();
+    Console.WriteLine("Primeiro valor:");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor:");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    float resultado = v1 * v2;
+
+    Console.WriteLine($"O resultado da multiplicação é { resultado }");
+
+    Console.ReadKey();
+}
