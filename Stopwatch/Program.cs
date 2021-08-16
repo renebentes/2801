50cc0faa7303fe40297a828fba9075e603858cf4
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-08-16 20:41:33 -0300

    Define a estrutura do cronômetro

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static void Start()
+{
+    int time = 10;
+    int currentTime = 0;
+
+    while (currentTime != time)
+    {
+        currentTime++;
+    }
+}
