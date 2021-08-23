b1148952a442c8d236ff7407166a31252f4858d2
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-08-23 20:22:58 -0300

    Cria estrutura inicial do Editor de Textos

---
+++ TextEditor/Program.cs
@@ -0,0 +1,32 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar arquivo");
+    Console.WriteLine("0 - Sair");
+
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0:
+            Environment.Exit(0);
+            break;
+        case 1:
+            Open();
+            break;
+        case 2:
+            Edit();
+            break;
+        default:
+            Menu();
+            break;
+    }
+}
+
+static void Open() { }
+
+static void Edit() { }
