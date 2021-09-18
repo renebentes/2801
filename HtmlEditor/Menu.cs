8c45fa9f6e60265b86f216ad9a4a8d3cbb075d7f
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-09-18 13:11:37 -0300

    Define métodos para desenhar uma caixa na tela

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -4,10 +4,61 @@ namespace HtmlEditor
 {
     public static class Menu
     {
+
+        private const string BottomLeftCorner = "└";
+        private const string BottomRightCorner = "┘";
+        private const string Dash = "-";
+        private const string Pipe = "|";
+        private const string TopLeftCorner = "┌";
+        private const string TopRightCorner = "┐";
+
         public static void Show()
         {
-            Console.BackgroundColor=ConsoleColor.Blue;
-            Console.ForegroundColor=ConsoleColor.White;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            DrawScreen();
+        }
+
+        private static void DrawScreen()
+        {
+            DrawTopBar();
+            DrawContent();
+            DrawBottomBar();
+        }
+
+        private static void DrawContent()
+        {
+            for (int lines = 0; lines < 10; lines++)
+            {
+                Console.Write(Pipe);
+                for (int columns = 0; columns < 30; columns++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write($"{Pipe}{Environment.NewLine}");
+            }
+        }
+
+        private static void DrawBottomBar()
+        {
+            Console.Write(BottomLeftCorner);
+            for (int columns = 0; columns < 30; columns++)
+            {
+                Console.Write(Dash);
+            }
+            Console.Write($"{BottomRightCorner}{Environment.NewLine}");
+        }
+
+        private static void DrawTopBar()
+        {
+            Console.Write(TopLeftCorner);
+            for (int columns = 0; columns < 30; columns++)
+            {
+                Console.Write(Dash);
+            }
+            Console.Write($"{TopRightCorner}{Environment.NewLine}");
         }
     }
 }
