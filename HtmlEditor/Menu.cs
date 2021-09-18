d22d817cf0c565aa54d382721da8e4f89743f9a8
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-09-18 11:30:16 -0300

    Cria estrutura do projeto para o editor de HTML

---
+++ HtmlEditor/Menu.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HtmlEditor
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.BackgroundColor=ConsoleColor.Blue;
+            Console.ForegroundColor=ConsoleColor.White;
+        }
+    }
+}
---
+++ HtmlEditor/Program.cs
@@ -0,0 +1,3 @@
+﻿using HtmlEditor;
+
+Menu.Show();
