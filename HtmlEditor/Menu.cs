97929db986765d8ffdc055090203e71f63b870cc
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-09-20 20:40:34 -0300

    Define menu de opções

--- HtmlEditor/Menu.cs
+++ HtmlEditor/Menu.cs
@@ -19,6 +19,9 @@ namespace HtmlEditor
             Console.ForegroundColor = ConsoleColor.White;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
 
         private static void DrawScreen()
@@ -59,6 +62,24 @@ namespace HtmlEditor
                 Console.Write(Dash);
             }
             Console.Write($"{TopRightCorner}{Environment.NewLine}");
+        }
+
+        private static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("===========");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção:");
         }
     }
 }
