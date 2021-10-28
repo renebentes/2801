b44162ddf738471058fa744a4de3867e9c2e5784
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-10-27 21:12:48 -0300

    :sparkles: Cria projeto para datas

---
+++ Dates/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.Clear();
+
+var dateTime = new DateTime();
+// structs não são nulos 01/01/0001 00:00:00
+Console.WriteLine(dateTime);
+
+dateTime = DateTime.Now;
+Console.WriteLine(dateTime);
