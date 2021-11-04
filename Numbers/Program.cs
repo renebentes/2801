5da412ebdf069fad8d540a86aba87ce35fac8a0f
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-11-04 10:17:53 -0300

    :sparkles: Formata número

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -1,6 +1,14 @@
-﻿Console.Clear();
+﻿using System.Globalization;
+
+Console.Clear();
 
 // 1ª opção para moeda (precisão), a menos que precise de performance (double ou float)
-decimal valor = 10.25m;
+decimal valor = 10536.25m;
 
-Console.WriteLine(valor);
+// G - formatação padrão de números
+// C - currency, moeda
+// E - notação científica
+// N - separador de milhar
+// P - porcentagem
+Console.WriteLine(
+    valor.ToString("N", CultureInfo.CreateSpecificCulture("pt-BR")));
