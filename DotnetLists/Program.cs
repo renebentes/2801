d92292c5ebf0cbf90bc4dd486ca29cad720957d2
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-11-04 11:38:38 -0300

    :sparkles: Cria projeto para arrays

---
+++ DotnetLists/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+var meuArray = new Teste[2];
+meuArray[0] = new Teste();
+
+Console.WriteLine(meuArray[0].Id);
+Console.WriteLine(meuArray[1]);
+
+struct Teste
+{
+    public int Id { get; set; }
+}
