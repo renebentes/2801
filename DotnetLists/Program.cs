22e43906f8fdc3394f3a4f9aacc3f8ee8af35146
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-11-08 20:33:01 -0300

    :sparkles: Lança exceção

--- DotnetLists/Program.cs
+++ DotnetLists/Program.cs
@@ -1,9 +1,13 @@
-﻿Console.Clear();
+﻿using System;
+
+Console.Clear();
 
 var arr = new int[3] { 1, 2, 3 };
 
 try
 {
+    Cadastrar(null);
+
     for (int i = 0; i < 10; i++)
     {
         // IndexOutOfRangeException
@@ -16,9 +20,23 @@ catch (IndexOutOfRangeException ex)
     Console.WriteLine(ex.Message);
     Console.WriteLine("Não encontrei o índice na lista");
 }
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex.InnerException);
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Falha ao cadastrar");
+}
 catch (Exception ex)
 {
     Console.WriteLine(ex.InnerException);
     Console.WriteLine(ex.Message);
     Console.WriteLine("Ops, algo deu errado!");
+}
+
+static void Cadastrar(string texto)
+{
+    if (string.IsNullOrEmpty(texto))
+    {
+        throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
+    }
 }
